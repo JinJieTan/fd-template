b59b52de7454e564b47f248a780a154d89f7e9fb
venchoi <chenwh03@mingyuanyun.com>
2020-04-24 10:14:54 +0800

    chore(styleguidist)

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './index.less';
 interface Props {
+    /** 点击回调 */
     onClick: () => void;
+    /** 按钮内容 */
     text: string;
-    isNormal: boolean;
+    /** 按钮内容 */
+    isNormal?: boolean;
 }
 
 class App extends React.PureComponent<Props> {
